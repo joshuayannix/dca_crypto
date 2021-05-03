c16e3586382d89f43fbe5c40b76c574ffd6f0385
Josh <joshuayannix@gmail.com>
2021-05-03 19:09:20 -0400

    price history pulled in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2018&localization=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> {coins.market_data.current_price}
         </p>
         <a
           className="App-link"
