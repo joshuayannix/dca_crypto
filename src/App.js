5b15c631c2af4c21d4145dbae01a68f7e9889dbc
Josh <joshuayannix@gmail.com>
2021-05-03 22:16:11 -0400

    datepicker and api price all working

--- src/App.js
+++ src/App.js
@@ -1,73 +1,62 @@
 import React, { useEffect, useState } from 'react'
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import {
-  DatePicker,
-  TimePicker,
-  DateTimePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
+import {DatePicker,MuiPickersUtilsProvider} from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [selectedDate, handleDateChange] = useState(new Date());
-
-  useEffect(() => {
-    axios
-      .get(
-        'https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2018&localization=false'
-      )
-      .then(res => {
-        setCoins(res.data.market_data.current_price.usd);
-        console.log(res.data);
-        console.log(res.data.market_data.current_price.usd)
-        console.log(res.data.market_data.market_cap.usd)
-      })
-      .catch(error => console.log(error));
-  }, []);
-
   const [price, setPrice] = useState([])
+
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       'https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2018&localization=false'
+  //     )
+  //     .then(res => {
+  //       setCoins(res.data.market_data.current_price.usd);
+  //       console.log(res.data);
+  //       console.log(res.data.market_data.current_price.usd)
+  //       console.log(res.data.market_data.market_cap.usd)
+  //     })
+  //     .catch(error => console.log(error));
+  // }, []);
+
   
-  const apiGet = () => {
-    fetch('https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2020&localization=false')
+  const apiGet = (crypto, day, month, year) => {
+    fetch(`https://api.coingecko.com/api/v3/coins/${crypto}/history?date=${day}-${month}-${year}&localization=false`)
     .then(res => res.json())
     .then(json => {
       console.log(json.market_data.current_price.usd)
       setPrice(json.market_data.current_price.usd)
     })
   }
-  const [date, setDate] = useState([]);
 
+  console.log(new Date())
   const onSubmit = e => {
     e.preventDefault()
-    apiGet()
+    apiGet('bitcoin',selectedDate.getMonth() + 1, selectedDate.getDate(), selectedDate.getFullYear())
   };
 
   return (
     <div className="App">
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <DatePicker 
-          value={selectedDate} 
-          onChange={handleDateChange} 
-          format='MM/dd/yyy'
-        />
-      </MuiPickersUtilsProvider>
+      
       <form onSubmit={onSubmit}>
-        
-        <label> Date</label>
-        <input
-          value={date}
-          onChange = {e=>setDate(e.target.value)}
-        />
+        <label>Material UI Date</label>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <DatePicker 
+            value={selectedDate} 
+            onChange={handleDateChange} 
+            format='MM/dd/yyy'
+          />
+        </MuiPickersUtilsProvider>
+
         <button type='submit'>Submit</button>
       </form>
 
 
-        <p> Historical price {coins}</p>
-        <button onClick={apiGet}> Press me to post some data
-        </button>
+                
         {price}
 
     </div>
