c8dd461ccfd7993cc3f050d1eae4afd78952077d
Josh <joshuayannix@gmail.com>
2021-05-03 21:23:40 -0400

    date picker material ui

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import {
+  DatePicker,
+  TimePicker,
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 function App() {
   const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState('');
+  const [selectedDate, handleDateChange] = useState(new Date());
 
   useEffect(() => {
     axios
@@ -31,21 +38,38 @@ function App() {
       setPrice(json.market_data.current_price.usd)
     })
   }
+  const [date, setDate] = useState([]);
+
+  const onSubmit = e => {
+    e.preventDefault()
+    apiGet()
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p> Historical price {coins}
-        </p>
-        <div>
-          <button onClick={apiGet}> Press me to post some data
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <DatePicker 
+          value={selectedDate} 
+          onChange={handleDateChange} 
+          format='MM/dd/yyy'
+        />
+      </MuiPickersUtilsProvider>
+      <form onSubmit={onSubmit}>
+        
+        <label> Date</label>
+        <input
+          value={date}
+          onChange = {e=>setDate(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
 
-          </button>
-        </div>
-        <div>
-          {price}
-        </div>
-      </header>
+
+        <p> Historical price {coins}</p>
+        <button onClick={apiGet}> Press me to post some data
+        </button>
+        {price}
+
     </div>
   );
 }
