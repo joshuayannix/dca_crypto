39361e1de9d87fc9b07422c2d0972fa255d929ae
Josh <joshuayannix@gmail.com>
2021-05-03 20:06:17 -0400

    fetch price button

--- src/App.js
+++ src/App.js
@@ -13,26 +13,38 @@ function App() {
         'https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2018&localization=false'
       )
       .then(res => {
-        setCoins(res.data);
+        setCoins(res.data.market_data.current_price.usd);
         console.log(res.data);
+        console.log(res.data.market_data.current_price.usd)
+        console.log(res.data.market_data.market_cap.usd)
       })
       .catch(error => console.log(error));
   }, []);
 
+  const [price, setPrice] = useState([])
+  
+  const apiGet = () => {
+    fetch('https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2020&localization=false')
+    .then(res => res.json())
+    .then(json => {
+      console.log(json.market_data.current_price.usd)
+      setPrice(json.market_data.current_price.usd)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> {coins.market_data.current_price}
+        <p> Historical price {coins}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button onClick={apiGet}> Press me to post some data
+
+          </button>
+        </div>
+        <div>
+          {price}
+        </div>
       </header>
     </div>
   );
